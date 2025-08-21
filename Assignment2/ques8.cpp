a032a00536f9cee67b5052ba4c3ace9275f5f39a
SagarikaWankhede <sagarika.wankhede@gmail.com>
2025-08-21 16:43:25 +0530

    ques8

---
+++ Assignment2/ques8.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter size of array: ";
+    cin >> n;
+
+    int arr[n];
+    cout << "Enter " << n << " elements: ";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    int distinctCount = 0;
+
+    for (int i = 0; i < n; i++) {
+        bool isDuplicate = false;
+        // check if arr[i] appeared before
+        for (int j = 0; j < i; j++) {
+            if (arr[i] == arr[j]) {
+                isDuplicate = true;
+                break;
+            }
+        }
+        if (!isDuplicate) {
+            distinctCount++;
+        }
+    }
+
+    cout << "Total number of distinct elements: " << distinctCount << endl;
+
+    return 0;
+}
