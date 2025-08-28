2b4f7630c0afa3e695e7bdb56fd3d97995182835
SagarikaWankhede <sagarika.wankhede@gmail.com>
2025-08-28 22:44:36 +0530

    ques7

---
+++ Assignment2/ques7.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int countInversions(int arr[], int n) {
+    int count = 0;
+    for (int i = 0; i < n-1; i++) {
+        for (int j = i+1; j < n; j++) {
+            if (arr[i] > arr[j]) count++;
+        }
+    }
+    return count;
+}
+
+int main() {
+    int n;
+    cout << "Enter size of array: ";
+    cin >> n;
+
+    int arr[100];
+    cout << "Enter array elements: ";
+    for (int i = 0; i < n; i++) cin >> arr[i];
+
+    cout << "Number of inversions: " << countInversions(arr, n) << endl;
+    return 0;
+}
