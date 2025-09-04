a7ffe999fea3585e014704683dc69ebac26a6cf0
SagarikaWankhede <sagarika.wankhede@gmail.com>
2025-09-05 00:18:58 +0530

    ques2

---
+++ Assignment 3/ques2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <stack>
+#include <string>
+using namespace std;
+int main() {
+    string str;
+    cout << "Enter a string: ";
+    cin >> str;   
+    stack<char> s;
+    for (int i = 0; i < str.length(); i++) {
+        s.push(str[i]);
+    }
+    string reversed = "";
+    while (!s.empty()) {
+        reversed += s.top();
+        s.pop();
+    }
+    cout << "Reversed string: " << reversed << endl;
+    return 0;
+}
